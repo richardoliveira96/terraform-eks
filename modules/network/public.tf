070301f98f6274fdd6f6b72e89054b573f2c10a3
Richard Oliveira <richard.oliveira@acad.pucrs.br>
2024-05-14 17:35:13 -0300

    fix: terraform fmt

--- modules/network/public.tf
+++ modules/network/public.tf
@@ -5,7 +5,7 @@ resource "aws_subnet" "eks_subnet_public_1a" {
   map_public_ip_on_launch = true
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                     = "${var.project_name}-pub-subnet-1a",
       "Kubernetes.io/role/elb" = 1
@@ -20,7 +20,7 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   map_public_ip_on_launch = true
 
   tags = merge(
-    local.tags,
+    var.tags,
     {
       Name                     = "${var.project_name}-pub-subnet-1b",
       "Kubernetes.io/role/elb" = 1
